47b85dee0d59bde9e6930f06b69d730be3f78d45
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2018-11-30 22:45:09 +0100

    Getting ducks in a row.

---
+++ Ebrahim/src/main/java/com/aharpour/adventofcode/Day1Question1.java
@@ -0,0 +1,12 @@
+package com.aharpour.adventofcode;
+
+public class Day1Question1 {
+
+    public static int calculate(String string) {
+        return 0;
+    }
+
+
+
+
+}
---
+++ Ebrahim/src/test/java/com/aharpour/adventofcode/Day1Question1Test.java
@@ -0,0 +1,25 @@
+package com.aharpour.adventofcode;
+
+import org.junit.Test;
+
+import static com.aharpour.adventofcode.Day1Question1.calculate;
+import static org.junit.Assert.assertEquals;
+
+public class Day1Question1Test {
+
+    @Test
+    public void givenCases() {
+        assertEquals(0, calculate(""));
+    }
+
+    @Test
+    public void edgeCases() {
+        assertEquals(0, calculate(""));
+    }
+
+    @Test
+    public void challenge() {
+        assertEquals(0, calculate(""));
+    }
+
+}
