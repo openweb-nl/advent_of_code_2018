6256d031170da8d41ac922c0d0cc9a44dc437b91
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2018-12-09 10:45:03 +0100

    Refactoring day 9.

--- Ebrahim/src/main/java/com/aharpour/adventofcode/Day9Question1.java
+++ Ebrahim/src/main/java/com/aharpour/adventofcode/Day9Question1.java
@@ -1,6 +1,8 @@
 package com.aharpour.adventofcode;
 
 
+import com.aharpour.adventofcode.utils.datastructue.IntStatefulLinkedList;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,7 +16,7 @@ public class Day9Question1 {
 
     public long calculate(String string) {
         parseInput(string);
-        MarbleGame marbleGame = new MarbleGame(lastMarbleWorth);
+        MarbleGame marbleGame = new MarbleGame();
         for (int i = 0; i < lastMarbleWorth; i++) {
             scores[i % scores.length] = scores[i % scores.length] + marbleGame.placeMarble();
         }
@@ -33,105 +35,28 @@ public class Day9Question1 {
     }
 
     class MarbleGame {
-        private Node first;
-        private Node last;
         private int marble = 1;
-        private Node currentMarble;
+        private IntStatefulLinkedList list = new IntStatefulLinkedList();
 
-
-        MarbleGame(int numberOfMarbles) {
-            first = new Node(null, 0, null);
-            currentMarble = first;
-            last = first;
+        private MarbleGame() {
+            list.insert(0);
         }
 
         int placeMarble() {
             int result = 0;
             if (marble % 23 != 0) {
-                forward(1);
-                insert(marble);
+                list.forward(1);
+                list.insert(marble);
             } else {
-                backward(7);
-                result = marble + remove();
+                list.backward(7);
+                result = marble + list.remove();
             }
             marble++;
             return result;
         }
 
-        private int remove() {
-            int value = currentMarble.item;
-            Node prev = currentMarble.prev;
-            Node next = currentMarble.next;
-            if (prev != null) {
-                prev.next = next;
-            } else {
-                first = next;
-            }
-            if (next != null) {
-                next.prev = prev;
-                currentMarble = next;
-            } else {
-                last = prev;
-                currentMarble = first;
-            }
-            return value;
-        }
-
-
-        private void insert(int value) {
-            Node next = currentMarble.next;
-            Node newNode = new Node(currentMarble, value, next);
-            if (next != null) {
-                next.prev = newNode;
-            } else {
-                last = newNode;
-            }
-            currentMarble.next = newNode;
-            currentMarble = newNode;
-        }
-
-        private Node forward(int steps) {
-            if (steps < 0) {
-                throw new IllegalArgumentException();
-            }
-            for (int s = 0; s < steps; s++) {
-                Node next = currentMarble.next;
-                if (next != null) {
-                    currentMarble = next;
-                } else {
-                    currentMarble = first;
-                }
-            }
-            return currentMarble;
-        }
-
-        private Node backward(int steps) {
-            if (steps < 0) {
-                throw new IllegalArgumentException();
-            }
-            for (int s = 0; s < steps; s++) {
-                Node prev = currentMarble.prev;
-                if (prev != null) {
-                    currentMarble = prev;
-                } else {
-                    currentMarble = last;
-                }
-            }
-            return currentMarble;
-        }
-
 
     }
 
-    private static class Node {
-        int item;
-        Node next;
-        Node prev;
 
-        Node(Node prev, int element, Node next) {
-            this.item = element;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
 }
---
+++ Ebrahim/src/main/java/com/aharpour/adventofcode/utils/datastructue/IntStatefulLinkedList.java
@@ -0,0 +1,85 @@
+package com.aharpour.adventofcode.utils.datastructue;
+
+public class IntStatefulLinkedList {
+
+    private Node first;
+    private Node last;
+    private Node currentMarble;
+
+    public int remove() {
+        int value = currentMarble.item;
+        Node prev = currentMarble.prev;
+        Node next = currentMarble.next;
+        if (prev != null) {
+            prev.next = next;
+        } else {
+            first = next;
+        }
+        if (next != null) {
+            next.prev = prev;
+            currentMarble = next;
+        } else {
+            last = prev;
+            currentMarble = first;
+        }
+        return value;
+    }
+
+    private static class Node {
+        int item;
+        Node next;
+        Node prev;
+
+        Node(Node prev, int element, Node next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void insert(int value) {
+        Node next = currentMarble != null ? currentMarble.next : null;
+        Node newNode = new Node(currentMarble, value, next);
+        if (next != null) {
+            next.prev = newNode;
+        } else {
+            last = newNode;
+        }
+        if (currentMarble == null) {
+            first = newNode;
+        } else {
+            currentMarble.next = newNode;
+        }
+        currentMarble = newNode;
+    }
+
+    public int forward(int steps) {
+        if (steps < 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int s = 0; s < steps; s++) {
+            Node next = currentMarble.next;
+            if (next != null) {
+                currentMarble = next;
+            } else {
+                currentMarble = first;
+            }
+        }
+        return currentMarble.item;
+    }
+
+    public int backward(int steps) {
+        if (steps < 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int s = 0; s < steps; s++) {
+            Node prev = currentMarble.prev;
+            if (prev != null) {
+                currentMarble = prev;
+            } else {
+                currentMarble = last;
+            }
+        }
+        return currentMarble.item;
+    }
+}
